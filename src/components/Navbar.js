f20628d7239e6273295add2e3975894440c12d7b
Kelly Ung <kellyung@myyahoo.com>
2024-08-07 18:58:37 -0700

    update nav bar based on user log in

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,19 +1,24 @@
 import './Navbar.css';
 import { Link } from 'react-router-dom';
+import Auth from './Auth'
 
-export default function Navbar() {
+export default function Navbar({ user }) {
     return (
         <nav className='nav'>
             <div className='site-title'>Recipe Saver</div>
                 <ul>
+                    {user && 
+                        <li>
+                            <Link to='/Recipes'>Recipes</Link>
+                        </li>
+                    }
+                    {user && 
+                        <li>
+                            <Link to='/RecipeSearch'>Search</Link>
+                        </li>
+                    }
                     <li>
-                        <Link to='/'>Home</Link>
-                    </li>
-                    <li>
-                        <Link to='/Recipes'>Recipes</Link>
-                    </li>
-                    <li>
-                        <Link to='/RecipeSearch'>Search</Link>
+                        <Auth />
                     </li>
                 </ul>
         </nav>
